4a2baae4b0fc540a6d544b0ae7ef421455ee4d4d
Ramone Robertson <ramonerobertson10@gmail.com>
2025-01-21 17:03:30 +0900

    Created book model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_21_073311) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_21_080307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.integer "rating"
+    t.text "overview"
+    t.text "review"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +37,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_21_073311) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "books", "users"
 end
