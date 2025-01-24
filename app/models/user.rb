da1a3090c46212e7e29ec58e4b17a089b55c3205
Ramone Robertson <ramonerobertson10@gmail.com>
2025-01-24 16:28:59 +0900

    added validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  enum role: { default: 0, admin: 1 }
+  validates :name, :email, presence: true
+  validates_uniqueness_of :email
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_secure_password
+
+  def admin?
+    role == "admin"
+  end
 end
--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-Book.destroy_all
-ApiToken.destroy_all
-User.destroy_all
-
-User.create!(email: "user@example.com", password: 123123)
